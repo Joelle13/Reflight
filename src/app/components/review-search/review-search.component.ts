796417fa8d679e4b48336839f63c391febfc8133
Joëlle <joelle.huyen13@hotmail.fr>
2025-10-30 02:08:53 +0100

    💄 Ajout du style sur la review page

---
+++ src/app/components/review-search/review-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-search',
+  templateUrl: './review-search.component.html',
+  styleUrl: './review-search.component.css'
+})
+export class ReviewSearchComponent {
+
+}
