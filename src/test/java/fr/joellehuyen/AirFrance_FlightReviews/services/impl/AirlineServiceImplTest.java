43fbc50fce661e8919506582e9ead427338d6f25
Joëlle <joelle.huyen13@hotmail.fr>
2025-10-31 23:43:43 +0100

    ⚡️ Ajout tests unitaires

---
+++ src/test/java/fr/joellehuyen/AirFrance_FlightReviews/services/impl/AirlineServiceImplTest.java
@@ -0,0 +1,45 @@
+package fr.joellehuyen.AirFrance_FlightReviews.services.impl;
+
+import fr.joellehuyen.AirFrance_FlightReviews.models.Airline;
+import fr.joellehuyen.AirFrance_FlightReviews.repositories.AirlineRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AirlineServiceImplTest {
+
+    @Mock
+    private AirlineRepository airlineRepository;
+
+    @InjectMocks
+    private AirlineServiceImpl airlineService;
+
+    @Test
+    void shouldReturnAllAirlines() {
+        Airline a1 = new Airline();
+        a1.setId("1");
+        a1.setName("AIR FRANCE");
+        Airline a2 = new Airline();
+        a2.setId("2");
+        a2.setName("DELTA");
+
+        given(airlineRepository.findAll()).willReturn(Arrays.asList(a1, a2));
+
+        List<Airline> result = airlineService.findAll();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("AIR FRANCE", result.getFirst().getName());
+        then(airlineRepository).should().findAll();
+    }
+
+}
